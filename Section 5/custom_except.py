a62a965431d524da89a8c9ae7dcca0c7d8a44b8f
jonnytaddesky <s.v.artem2020@gmail.com>
2021-04-13 13:03:54 +0300

    automatic test

---
+++ Section 5/custom_except.py
@@ -0,0 +1,47 @@
+import math
+
+# def calc_square(ab, bc, ac):
+#     p = (ab + bc + ac) / 2
+#     s = math.sqrt(p * (p - ab)* (p - bc) * (p - ac))
+
+#     return s
+
+# square = calc_square(10, 10, 10)
+# print(square)
+
+# square = calc_square(-2, 8, 8)
+# print(square)
+
+
+# def calc_square(ab, bc, ac):
+#     if ab <= 0 or bc <= 0 or  ac <= 0:
+#         raise ValueError("One of the sides is les or equal to 0.")
+
+#     p = (ab + bc + ac) / 2
+#     s = math.sqrt(p * (p - ab)* (p - bc) * (p - ac))
+
+#     return s
+
+
+# square = calc_square(-2, 8, 8)
+# print(square)
+
+
+class InvalidTriangleError(Exception):
+    """Raised when a triangle has invalid sides"""
+
+def calc_square(ab, bc, ac):
+    if ab <= 0 or bc <= 0 or  ac <= 0:
+        raise InvalidTriangleError("One of the sides is less or equal to 0.")
+
+    p = (ab + bc + ac) / 2
+    s = math.sqrt(p * (p - ab)* (p - bc) * (p - ac))
+
+    return s
+
+try:
+    square = calc_square(10, 8, 8)
+except InvalidTriangleError as ex:
+    print(ex)
+else:
+    print(square)
---
+++ Section 5/errors.py
@@ -0,0 +1,40 @@
+# def divide(a, b):
+#     try:
+#         return a / b
+#     except ZeroDivisionError as ex:
+#         print(f'an error occured: {ex}')
+#     except:
+#         print('unknow error occured!')
+
+# print(divide(4,2))
+# divide(4,0)
+
+# divider = input()
+# divide(4, divider)
+
+# file = None
+# try:
+#     file = open(r'C:\tmp\abracadabra.txt')
+#     data  = file.read()
+# except FileNotFoundError as ex:
+#     print(f'Error has occured. Discription: {ex.strerror}')
+# else:
+#     print('возможно else')
+# finally:
+#     print('Finally')
+#     if file:
+#         file.close()
+
+# print('doing some work here')
+
+
+def get_int():
+    while True:
+        try:
+            reply = int(input('Enter a number: '))
+            return reply
+        except:
+            print('Not a number! Try again.')
+            continue
+
+get_int()
