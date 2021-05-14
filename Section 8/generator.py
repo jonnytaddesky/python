7b93b43d36859d0fa885c29e54aad6851b399c90
jonnytaddesky <s.v.artem2020@gmail.com>
2021-05-14 11:37:15 +0300

    change file

--- Section 8/generator.py
+++ Section 8/generator.py
@@ -1,5 +1,5 @@
 import random
-
+import itertools
 
 # def randoms(min, max, n):
 #     return [random.randint(min, max) for _ in range(n)]
@@ -13,8 +13,8 @@ def randoms(min, max, n):
         yield random.randint(min, max)
 
 
-for r in randoms(10, 30, 5):
-    print(r)
+# for r in randoms(10, 30, 5):
+#     print(r)
 
 rand_sequence = randoms(1, 10, 10)
 print(rand_sequence)
@@ -26,3 +26,40 @@ seq = list(randoms(1, 10, 5))
 print(seq)
 print(seq)
 print(seq)
+
+rand_sequence = randoms(1, 10, 10)
+five_taken = list(itertools.islice(rand_sequence, 5))
+print(five_taken)
+
+
+def randoms(min, max):
+    while True:
+        yield random.randint(min, max)
+
+
+rand_sequence = randoms(1, 100000)
+five_taken = list(itertools.islice(rand_sequence, 5))
+print(five_taken)
+
+
+def read_line_by_line(file):
+    """Lazy function (genreation) to read a file line by line"""
+    while True:
+        line = file.readline()
+        if not line:
+            break
+        yield line
+
+
+file = open(f'C:\\tmp\\test.txt')
+for line in read_line_by_line(file):
+    print(line.rstrip())
+
+
+rand_seq = randoms(1, 10)
+n = next(rand_seq)
+print(n)
+n = next(rand_seq)
+print(n)
+n = next(rand_seq)
+print(n)
